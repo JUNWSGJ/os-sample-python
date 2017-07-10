2828c2c66a59e84faef94013017fb0e3e666df85
JUN <15850761665@163.com>
2017-07-10 23:50:26 +0800

    test webhook

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
+from pymongo import MongoClient
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({'msg': 'success'})
+
+@application.route("/mongo/search")
+def test_mongo():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.goomoo
+    count = db.users.find().count()
+    print('count:', count)
+    return 'count:{0}\n'.format(count)
+
+@application.route("/mongo/add")
+def test_mongo_add():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.goomoo
+    users = db.users
+    users.insert_one({'name':'张三', 'age':18})
+    return "succcess!"
+
 
 @application.route("/env")
 def get_env():
